dcfeb348328eb1ef968d8545cbb031a318dae384
Christalyn Maramag <cristal.maramag@gmail.com>
2020-11-19 13:47:22 +0800

    cambridge ol exam

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// input 1
+function increment1() {
+    document.getElementById('demoInput1').stepUp();
+}
+function decrement1() {
+    document.getElementById('demoInput1').stepDown();
+}
+// input 2
+function increment2() {
+    document.getElementById('demoInput2').stepUp();
+}
+function decrement2() {
+    document.getElementById('demoInput2').stepDown();
+}
+// input 3
+function increment3() {
+    document.getElementById('demoInput3').stepUp();
+}
+function decrement3() {
+    document.getElementById('demoInput3').stepDown();
+}
